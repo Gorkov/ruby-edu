7b37df3c7485b7b46ee72d80a8bcb61a63da014a
gorkov <gorkov.as@yandex.ru>
2020-03-11 06:25:15 +0400

    add logic to triangle area script in 01 lesson

--- 01/right_triangle.rb
+++ 01/right_triangle.rb
@@ -7,4 +7,18 @@ second_side = gets.chomp.to_f
 puts 'Enter third side of the triangle'
 third_side = gets.chomp.to_f
 
-answer = 'This triangle is isosceles rectangular equilateral'
+all_sides = [first_side, second_side, third_side].sort!
+
+answer = 'This triangle is '
+
+if all_sides.uniq.size == 1
+  answer += 'isosceles and equilateral, but not rectangular'
+else
+  types = []
+  types << 'rectangular' if (all_sides[0]**2) + (all_sides[1]**2) == (all_sides[2]**2)
+  types << 'isosceles' if all_sides[0] == all_sides[1]
+
+  answer += types.join(' and ')
+end
+
+puts answer += '.'

03684b95938fb54901c264d479aee18789f10eb3
Anatoliy Gorkov <gorkov@mercurydevelopment.com>
2020-05-14 09:23:37 +0400

    LES-2-1 added resolv

--- 02/hash_and_months.rb
+++ 02/hash_and_months.rb
@@ -1,5 +1,8 @@
 require 'date'
 
-Date.civil(Date.today.year, 1, -1)..Date.civil(Date.today.year, 12, -1).each do |month|
-  # TODO condition of last day
+month_info = {}
+(Date.civil(Date.today.year, 1, -1)..Date.civil(Date.today.year, 12, -1)).map do |month|
+  month_info[month.strftime('%b')] = month.strftime('%d')
 end
+
+month_info.each { |month, last_date| puts month if last_date == '30'}

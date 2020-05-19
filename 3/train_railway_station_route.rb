1b94f5f04334788140c47ca36880ef9914c5caf6
Anatoliy Gorkov <gorkov@mercurydevelopment.com>
2020-05-19 09:02:41 +0400

    LES-3 added RailwayStation class

---
+++ 3/train_railway_station_route.rb
@@ -0,0 +1,24 @@
+class Train
+
+end
+
+class RailwayStation
+  attr_reader :trains, :name
+
+  def initialize(name)
+    @name = name
+    @trains = {}
+  end
+
+  def trains_by_type
+
+  end
+
+  def take_trains(train)
+    @trains.merge(train)
+  end
+
+  def send_trains(train)
+    @trains.delete(train)
+  end
+end

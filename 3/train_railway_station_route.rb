6f538bbb660163c94c5e3f961b52fb37f62d339a
Anatoliy Gorkov <gorkov@mercurydevelopment.com>
2020-06-02 07:56:05 +0400

    LES-3 updated Route class

--- 3/train_railway_station_route.rb
+++ 3/train_railway_station_route.rb
@@ -1,18 +1,30 @@
 class Route
   def initialize(station_list = [])
-    @stations_list = station_list
+    @station_list = station_list
   end
 
   def add_station(station)
-    @stations_list << station
+    @station_list << station
   end
 
   def remove_station
-    @stations_list.delete(station)
+    @station_list.delete(station)
+  end
+
+  def first_station
+    @station_list.first
+  end
+
+  def last_station
+    @station_list.last
+  end
+
+  def interim_stations
+    @station_list[1..-2]
   end
 
   def show_list
-    p @stations_list
+    puts @station_list
   end
 end
 

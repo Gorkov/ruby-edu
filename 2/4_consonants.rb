cbd67106432094b71626002d7ff8c43a10911be8
Gorkov <gorkov.as@yandex.ru>
2022-02-27 13:37:04 +0400

    LES-2-4 updated task resolving

--- 2/4_consonants.rb
+++ 2/4_consonants.rb
@@ -1,9 +1,3 @@
-consonants = %w[ a e i o u ]
-
-result = {}
-
-('a'..'z').to_a.each_with_index do |value, index|
-  result[value] = index + 1 if consonants.include? value
-end
-
-p result
+hash = ('a'..'z').each_with_index.map do |letter, index|
+  [letter, index + 1] if %w(a e i o u).include?(letter)
+end.compact.to_h
